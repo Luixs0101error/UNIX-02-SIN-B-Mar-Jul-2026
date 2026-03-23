d54948e2b03b5a53362fdd21f7317334de8a16cd
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-03-23 12:58:47 +0000

    importación llave publica

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -1,4 +1,8 @@
 #Mostrar clave privada
 gpg --list-secret-keys --keyid-format=long
 #Exportar llave privada
-gpg --armor --export-secret-keys 
+gpg --armor --export-secret-keys DFA1A9B4A5C355EE
+#Listar todas mis llaves
+gpg --list-keys
+#Importar llave publica mi amigo
+gpg --import MiCompa.asc
