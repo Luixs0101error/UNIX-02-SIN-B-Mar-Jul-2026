8c79ad5a71d82843415a3c6d590cf5eb88764f24
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-03-25 13:44:28 +0000

    Shows detailed information about the test.txt

--- filesystem.sh
+++ filesystem.sh
@@ -2,3 +2,20 @@ pwl #Print working directory
 cd #change directory
 ls -F #List the files and folders
 ls -i #Go to the inodos 
+
+#Enlace duro(Hark link)
+#Enlace simbólico (Soft link)
+#Puntero guarda la dirección de memoria
+
+bin@ #Ejecutable básico
+sbin@ #Ejecutables supervisores
+lib@ #librería del sistema
+
+vmlinuz@ y vmlinuz.old@ #Kernel de linux
+initrid #Carga drivers básicos
+run #Datos del sistema en ejecución
+tmp #archivos temporales generales
+
+#Bloque es la unidad minima de almacenamiento
+
+stat test.txt #Shows detailed information about the test.txt
