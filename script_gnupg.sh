4c0d617f0bc2a3538873fe01c77b33c642a52c4e
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-03-25 01:03:24 +0000

    Sign the key of our decision

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -25,4 +25,6 @@ gpg output firma_separada_doc_no_cifrado.sig doc_no_cifrado.txt
 #Verificar firma de mi compañero
 gpg --verify doc_no_cifrado_firmado_jeanellafirmado.txt
 #Web of trust to our partner
-gpg --edit-key 7ABA372FA126A7CBFCD64DC73B2DD130907A2F2E
+gpg --edit-key 7ABA372FA1S26A7CBFCD64DC73B2DD130907A2F2E
+#ign the key of our decision
+gpg --sign-key 3B2DD130907A2F2E
