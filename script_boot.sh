fb409bd87ce70763a838a94c13c8a4276e07a4c6
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-04-06 12:50:52 +0000

    Details and instructions with Device

--- script_boot.sh
+++ script_boot.sh
@@ -12,7 +12,14 @@ mkdir proyecto
 cd proyecto/
 #List all files and directories
 ls -lai
-#
+#Result of the list of the current directory and hark link
 total 12
 925551 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:34 .
 918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:34 ..
+#Show detailed information about the current directory
+stat .
+#indicates the device ID where the file or directory is stored
+#The first number (0) is the major device number.
+#The second number (45) is the minor device number.
+Device: 0,45
+
