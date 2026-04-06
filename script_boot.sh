78b2d917364655f3dd36c9733148daf97146a5ab
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-04-06 12:32:53 +0000

    cd_use

---
+++ script_boot.sh
@@ -0,0 +1,8 @@
+#Change directory to your home directory
+cd
+#Change directory to /home/codespace
+cd /home/codespace
+#Change directory to your home directory using the ~ shortcut
+cd ~
+#Change directory to your home directory using the $HOME environment variable
+cd $HOME
