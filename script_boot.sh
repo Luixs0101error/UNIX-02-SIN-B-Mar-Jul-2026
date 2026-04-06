47a9a9c8215c05eee8c87c1c8ab6b57178b2d379
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-04-06 12:39:28 +0000

    create_the_directory

--- script_boot.sh
+++ script_boot.sh
@@ -6,3 +6,13 @@ cd /home/codespace
 cd ~
 #Change directory to your home directory using the $HOME environment variable
 cd $HOME
+#Create the directory "proyecto"
+mkdir proyecto
+#Change to directory "proyecto"
+cd proyecto/
+#List all files and directories
+ls -lai
+#
+total 12
+925551 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:34 .
+918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:34 ..
