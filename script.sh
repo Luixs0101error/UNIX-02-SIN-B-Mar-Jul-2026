16789c0ab60d3c83f5f3ed4f573ac2ec2c4ab0b2
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-04-13 12:40:54 +0000

    list of a directory

--- script.sh
+++ script.sh
@@ -1,8 +1,14 @@
-#COMANDO-
+#COMANDO
 #OPCIONES
-# ARGUMENTS
+#ARGUMENTOS
 
 ls [OPTIONS] [PATH]    # general syntax of the ls command
 ls -a                  # short option: shows all files, including hidden ones
 ls --all               # long option: equivalent to -a, shows all files
 ls -a /                # shows all files (including hidden ones) in the root directory /
+
+ls -l -a -h    # lists files in long format (-l), includes hidden files (-a), and shows sizes in human-readable format (-h)
+ls -l -ah      # same as above, combined options (-l -a -h)
+ls -lah        # same as above, all options combined in one
+ls -l          # lists files in long format (permissions, owner, size, date)
+ls -l -ah      # lists in long format, includes hidden files, and shows human-readable sizes
