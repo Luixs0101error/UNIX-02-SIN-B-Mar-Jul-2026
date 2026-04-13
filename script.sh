c4b0d97bbe70f57d13eb92718053fb81c2a35eac
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-04-13 12:33:24 +0000

    Structure of a command

---
+++ script.sh
@@ -0,0 +1,8 @@
+#COMANDO-
+#OPCIONES
+# ARGUMENTS
+
+ls [OPTIONS] [PATH]    # general syntax of the ls command
+ls -a                  # short option: shows all files, including hidden ones
+ls --all               # long option: equivalent to -a, shows all files
+ls -a /                # shows all files (including hidden ones) in the root directory /
