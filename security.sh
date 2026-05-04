a615e5d21099bbc1ba7ff39c151d0bf7382e200d
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-05-04 13:36:01 +0000

    use to id, cat

--- security.sh
+++ security.sh
@@ -14,3 +14,26 @@ groups
 groups $USER
 # ES: Muestra los grupos a los que pertenece el usuario especificado en la variable $USER.
 # EN: Displays the groups that the user specified by the $USER variable belongs to.
+
+id -u
+# ES: Muestra el UID (User ID) del usuario actual.
+# EN: Displays the UID (User ID) of the current user.
+
+id -g
+# ES: Muestra el GID (Group ID) del grupo principal del usuario actual.
+# EN: Displays the GID (Group ID) of the current user's primary group.
+
+id -G
+# ES: Muestra todos los IDs de los grupos a los que pertenece el usuario actual.
+# EN: Displays all group IDs that the current user belongs to.
+
+cat /etc/group | grep codespace
+# ES: Muestra la información del grupo llamado "codespace" desde el archivo /etc/group.
+# EN: Shows information about the "codespace" group from the /etc/group file.
+
+cat /etc/gshadow
+# ES: Muestra el archivo /etc/gshadow, que contiene información sensible
+#     sobre grupos, como contraseñas y administradores.
+# EN: Displays the /etc/gshadow file, which contains sensitive group
+#     information such as passwords and group administrators.
+
