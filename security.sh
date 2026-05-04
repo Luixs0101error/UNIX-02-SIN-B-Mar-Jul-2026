4b12d177947aeaab8a3c6c757443774a399745df
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-05-04 13:26:57 +0000

    groups and groups $USER

--- security.sh
+++ security.sh
@@ -6,3 +6,11 @@ cat /etc/passwd | head -10
 # ES: Muestra las primeras 10 líneas del archivo /etc/passwd, que contiene las cuentas de usuario del sistema.
 # EN: Shows the first 10 lines of the /etc/passwd file, which contains system user accounts.
 
+
+groups
+# ES: Muestra los grupos a los que pertenece el usuario actual.
+# EN: Displays the groups the current user belongs to.
+
+groups $USER
+# ES: Muestra los grupos a los que pertenece el usuario especificado en la variable $USER.
+# EN: Displays the groups that the user specified by the $USER variable belongs to.
