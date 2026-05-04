fd7262826c68d85931d8613745bc4ade9eb7edd8
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-05-04 12:47:55 +0000

    user_id and cat

---
+++ security.sh
@@ -0,0 +1,8 @@
+id
+#ES: Muestra la información del usuario actual (UID, GID y grupos).
+# EN: Displays information about the current user (UID, GID, and groups).
+
+cat /etc/passwd | head -10
+# ES: Muestra las primeras 10 líneas del archivo /etc/passwd, que contiene las cuentas de usuario del sistema.
+# EN: Shows the first 10 lines of the /etc/passwd file, which contains system user accounts.
+
