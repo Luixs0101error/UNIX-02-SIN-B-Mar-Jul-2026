48b48a556a384f9a29e4b289d7cd3f6a9296bfdc
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-05-27 13:06:56 +0000

    #Create a file before newgrp

--- security2.sh
+++ security2.sh
@@ -5,3 +5,9 @@ gn # only the primary group name
 touch ~/test_inherited_group.txt
 ls -la ~/test_inherited_group.txt
 #The group is the user's primary group
+
+#View the current primary group
+echo "Grupo actual: $(id -gn)"
+#Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt 
