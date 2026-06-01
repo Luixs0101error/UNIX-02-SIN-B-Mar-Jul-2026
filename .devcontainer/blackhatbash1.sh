83a680e637b57de6241b125a0a807c353208d2ea
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-06-01 13:11:43 +0000

    bash -x, -r, -n

--- .devcontainer/blackhatbash1.sh
+++ .devcontainer/blackhatbash1.sh
@@ -11,4 +11,6 @@ echo ${OSTYPE}
 ps -e -f
 ps -ef
 df --human-readable
-bash -r blackhatbash1.sh # -r To run restricted mode
+#bash -r blackhatbash1.sh # To run restricted mode
+#bash -n blackhatbash1.sh # Displays syntaxs error
+#bash -x blackhaybash1.sh # 
