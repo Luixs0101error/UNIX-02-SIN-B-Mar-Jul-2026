aa0ca2282be47ec796a86730ba55355c7613ce97
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-06-01 13:17:56 +0000

    set -x and +x

--- .devcontainer/blackhatbash1.sh
+++ .devcontainer/blackhatbash1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #!/bin/bash -x  DISPLAYS ALL COMMANDS IN REAL TIME
-
+set -x #Enables trace mode. Bash prints each command before executing it.
 
 bash --version
 env
@@ -14,3 +14,5 @@ df --human-readable
 #bash -r blackhatbash1.sh # To run restricted mode
 #bash -n blackhatbash1.sh # Displays syntaxs error
 #bash -x blackhaybash1.sh # 
+
+set +x #Disables trace mode.
