ed29a8b4021c9dc7b5c1a5a3af113296894fdc65
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-06-08 12:51:17 +0000

    use to ping -c 3

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -4,7 +4,9 @@ SCRIPT_NAME="${0}"
 TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
-ping "${TARGET}"
+ping -c 3 "${TARGET}"
 
 chmod u+x ping_with_arguments.sh
 ./ping_with_arguments.sh nostarch.com
+
+ping -c 3 "${TARGET}"
