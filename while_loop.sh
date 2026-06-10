83d2b6b744de4a2023ea12b85a5772b6e3faf01b
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-06-10 13:48:58 +0000

    comments the stoploop

--- while_loop.sh
+++ while_loop.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 SIGNAL_TO_STOP_FILE="stoploop"
-while [[ ! -f  "${SIGNAL_TO_STOP_FILE}" ]]; do
- echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exis
-t..."
+# Defines a variable with the name of the file that will stop the loop
+
+while [[ ! -f "${SIGNAL_TO_STOP_FILE}" ]]; do
+# Starts a loop that continues while the file does NOT exist (-f checks if it is a file, ! negates it)
+
+ echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exist..."
+ # Prints a message saying the file has not been created yet
+
  echo "Checking again in 2 seconds..."
+ # Prints a message indicating the script will wait before checking again
+
  sleep 2
+ # Pauses execution for 2 seconds before the next iteration
+
 done
+# Ends the while loop (it stops when the file exists)
+
 echo "File was found! Exiting..."
+# Prints a message when the file is finally detected and the loop ends
