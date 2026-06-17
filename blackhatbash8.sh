31573664fab81f48907c21400fcd6fb8d0fb8351
Luixs0101error <luquisnancelaza@uide.edu.ec>
2026-06-17 13:17:35 +0000

    stop the proccess with Ctrl+Z

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -50,3 +50,14 @@ ps -ef | grep sleep
 jobs
 #[1]+  Hecho                      sleep 100
 
+fg %1
+#sleep 100
+
+fg %1
+#sleep 100
+#slepp Ctrl+Z
+#[1]+  Detenido                   sleep 100
+
+ bg %1
+#[1]+ sleep 100 &
+#[1]+  Hecho                      sleep 100
